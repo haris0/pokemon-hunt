dd0b75457f70e70d3d04ad4d0a6b7e5ba40b8ba9
Hari Surrisyad <hari.surrisyad@gmail.com>
2021-08-22 11:28:32 +0700

    implement appolo client and get list pokemon

--- src/component/MainPage/MainPage.jsx
+++ src/component/MainPage/MainPage.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+
+import {GET_POKEMONS} from '../../queries'
 
 function MainPage() {
+  
+  const { loading, error, data } = useQuery(GET_POKEMONS, {
+    variables: {
+        limit: 20,
+        offset: 1,
+      },
+  });
+
   return (
     <div>
-      Home
+      {loading && 
+        <div>Loading...</div>
+      }
+      {error && 
+        <div>
+          Error Load Data
+        </div>
+      }
+      {!loading && data && 
+        data.pokemons.results.map(pokemon => (
+          <div key={pokemon.name}> {pokemon.name} </div>
+        ))
+      }
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache
+} from '@apollo/client'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri:'https://graphql-pokeapi.vercel.app/api/graphql'
+  })
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
